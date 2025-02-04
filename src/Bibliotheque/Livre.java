024e031b40ea39ad2c984dbe0316120682a141c7
Mathis Lauriol-Torcq <mathis.lauriol-torcq@etu.univ-amu.fr>
2025-02-04 14:13:18 +0100

    Creation de la classe livre avec sa classe test

---
+++ src/Bibliotheque/Livre.java
@@ -0,0 +1,29 @@
+package Bibliotheque;
+
+public class Livre {
+	private String Name;
+	private double Price;
+	private int Quantity;
+	
+	public Livre() {
+		Name = "Harry Potter";
+		Price = 25.99;
+		Quantity = 12;
+	}
+	
+	public String getName() {
+		return Name;
+	}
+	
+	public double getPrice() {
+		return Price;
+	}
+	
+	public int getQuantity() {
+		return Quantity;
+	}
+	
+	public String toString() {
+		return "Livre[Name=" + Name + ",Price=" + Price + ",Quantity=" + Quantity + "]";
+	}
+}
---
+++ src/Bibliotheque/TestLivre.java
@@ -0,0 +1,14 @@
+package Bibliotheque;
+
+public class TestLivre {
+
+	public static void main(String[] args) {
+		Livre l1 = new Livre();
+	    System.out.println("Le livre s'appelle " 
+	    + l1.getName() + ", il coute " + l1.getPrice() + ", et il y a " +
+	    		l1.getQuantity() + "Exemplaire");
+	      
+	    System.out.println(l1.toString());
+	}
+
+}
